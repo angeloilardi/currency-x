d6947cd1e92f673759fca23a453060678e98bbb2
Angelo Ilardi <ange448@gmail.com>
2024-02-04 21:41:23 +0000

    added historical rate feature

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,7 @@
 import { PUBLIC_API_KEY } from '$env/static/public'
 
-// export function load() {
-    
-// }
-
 export const actions = {
-    default: async ({ request }) => {
+    getConversionRate: async ({ request }) => {
         // read the form data sent by the browser
         const formData = await request.formData()
         const baseCurrency = formData.get('base-currency');
@@ -19,10 +15,8 @@ export const actions = {
                 throw new Error(`HTTP error: ${response.status}`)
             }
             const result = await response.json();
-            console.log(baseCurrency);
 
             const conversionRate = result.data[targetCurrency];
-            console.log(conversionRate);
 
             return {
                 success: true, baseCurrency,
@@ -36,41 +30,45 @@ export const actions = {
         }
     },
 
-    // historical: async ({ request }) => {
-    //     const formData = await request.formData()
-    //     const baseCurrency = formData.get('base-currency');
-    //     const targetCurrency = formData.get('target-currency');
-    //     const date = formData.get('date');
-    //     try {
-    //         const response = await fetch(`https://api.freecurrencyapi.com/v1/historical?apikey=${PUBLIC_API_KEY}&currencies=${targetCurrency}&base_currency=${baseCurrency}&date=${date}`
+    historical: async ({ request, url }) => {
+        
+        const formData = await request.formData()
+        console.log(formData);
+        const baseCurrency = formData.get('base-currency');
+        const amountToConvert = formData.get('amount-to-convert');
+        const targetCurrency = formData.get('target-currency');
+        const date = formData.get('date');
+        const conversionRate = url.searchParams.get('conversion-rate')
+        console.log(date);
+        try {
+            const response = await fetch(`https://api.freecurrencyapi.com/v1/historical?apikey=${PUBLIC_API_KEY}&currencies=${targetCurrency}&base_currency=${baseCurrency}&date=${date}`
 
-    //         )
-    //         if (!response.ok) {
-    //             throw new Error(`HTTP error: ${response.status}`)
-    //         }
-    //         const result = await response.json();
-    //         console.log(result);
-    //         console.log(baseCurrency);
-    //         console.log(targetCurrency);
-    //         console.log(date);
-    //         console.log(Object.values(result));
+            )
+            if (!response.ok) {
+                throw new Error(`HTTP error: ${response.status}`)
+            }
+            const result = await response.json();
+            console.log(result);
+            console.log(baseCurrency);
+            console.log(targetCurrency);
+            console.log(date);
+            console.log(Object.values(result));
 
 
-    //         const historicalRate = result.data[date][targetCurrency];
-    //         console.log(historicalRate);
+            const historicalRate = result.data[date][targetCurrency];
+            console.log(historicalRate);
 
-    //         return {
-    //             success: true, 
-    //             baseCurrency,
-    //             targetCurrency,
-    //             conversionRate,
-    //             amountToConvert,
-    //             date, historicalRate
-    //         };
-    //     }
-    //     catch (error) {
-    //         console.error(error)
-    //         return { error: 'Unable to fetch currencies' }
-    //     }
-    // }
+            return {
+                success: true, 
+                date, historicalRate,
+                conversionRate,
+                baseCurrency, targetCurrency,
+                amountToConvert
+            };
+        }
+        catch (error) {
+            console.error(error)
+            return { error: 'Unable to fetch currencies' }
+        }
+    }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,14 @@
 	import { enhance } from '$app/forms';
 	import verticalSwap from '$lib/images/swap-vertical-sharp.svg';
 	import horizontalSwap from '$lib/images/swap-horizontal-sharp.svg';
+	import time from '$lib/images/time.svg';
+	import { fade, scale, blur } from 'svelte/transition';
+	import { quintOut } from 'svelte/easing';
+
+	$: console.log(form);
+	$: console.log(conversionRate);
+	$: historicalRate = form?.historicalRate;
+	let date = form?.date ?? null;
 
 	$: conversionRate = form?.conversionRate;
 
@@ -14,9 +22,9 @@
 
 	let myForm: HTMLFormElement;
 
-	let baseCurrency = form?.baseCurrency ?? null;
+	$: baseCurrency = form?.baseCurrency ?? null;
 
-	let targetCurrency = form?.targetCurrency ?? null;
+	$: targetCurrency = form?.targetCurrency ?? null;
 
 	let amountToConvert = form?.amountToConvert ?? null;
 
@@ -26,15 +34,12 @@
 
 	$: convertedAmount = amountToConvert * conversionRate;
 
-	console.log(conversionRate);
-
 	function fetchData() {
 		if (!targetCurrency || !baseCurrency) {
 			return;
 		} else {
 			console.log(baseCurrency);
 			myForm.submit();
-			console.log('called');
 		}
 	}
 
@@ -60,22 +65,22 @@
 		<div class="flex h-full items-center justify-center">
 			<div class="flex basis-2/3 flex-col justify-center">
 				<div class="h-[42px] w-[258px] font-piazzolla text-2xl font-bold tracking-wider text-white">
-					<a href="/">🅲🆄🆁🆁🅴🅽🅲🆈-🆇</a>
+					<a href="/"> <span class="rotate-180">🅲</span>🆄🆁🆁🅴🅽🅲🆈-🆇</a>
 				</div>
 				<h1
-					class="h-[78px] w-[196px] text-center font-piazzolla text-base font-semibold text-white"
+					class="h-[78px] w-[196px] text-justify font-piazzolla text-base font-semibold text-white"
 				>
 					Real time exchange rates at your fingertips
 				</h1>
 			</div>
-			<p class="text-5xl">💸</p>
+			<p class="text-5xl hover:animate-spin">💸</p>
 		</div>
 	</div>
 </header>
 
 <section class="mx-auto mt-10 md:max-w-4xl">
 	<form
-		action="/"
+		action="?/getConversionRate"
 		id="myform"
 		method="POST"
 		class="mx-auto max-w-[80%]"
@@ -83,8 +88,10 @@
 		bind:this={myForm}
 		on:submit|preventDefault
 	>
+		<!-- currencies input -->
 		<div class="flex w-full flex-col gap-3 md:flex-row">
 			<div class="w-full">
+				<!-- base currency input -->
 				<label for="base-currency" class="mb-2 block text-white">Select Base Currency</label>
 				<select
 					name="base-currency"
@@ -98,6 +105,7 @@
 					{/each}
 				</select>
 			</div>
+			<!-- swap button -->
 			<button
 				class="hidden h-6 w-6 shrink-0 self-end rounded-md bg-white p-1 hover:bg-slate-200 md:block"
 				type="button"
@@ -110,6 +118,7 @@
 				on:click|preventDefault={handleSwap}
 				><img src={verticalSwap} alt="" /><span class="sr-only">Swap currencies</span></button
 			>
+			<!-- target currency button -->
 			<div class="w-full">
 				<label for="target-currency" class="mb-2 block text-white">Convert to</label>
 				<select
@@ -125,13 +134,14 @@
 				</select>
 			</div>
 		</div>
-
-		{#if form}
-			<p class="mt-5 text-white italic">
-				Conversion rate: 1 {baseCurrency} = {conversionRate.toFixed(6)}
+		<!-- conversion rate -->
+{#if form}
+			<p transition:blur class="mt-5 italic text-white">
+				Conversion rate: 1 {baseCurrency} = {Number(conversionRate).toFixed(6)}
 				{targetCurrency}
 			</p>
 		{/if}
+		<!-- amount conversion -->
 		{#if form}
 			<div class="flex flex-col justify-between gap-6 md:flex-row">
 				<div class="mt-5">
@@ -153,22 +163,41 @@
 				{#if amountToConvert}
 					<div class="md:justify-end md:self-end">
 						<p class="mt-5 text-white">
-							{baseCurrency}
-							{formattedAmount} = {formatter.format(convertedAmount)}
+							{formattedAmount}
+							{baseCurrency} = {formatter.format(convertedAmount)}
 							{targetCurrency}
 						</p>
 					</div>
 				{/if}
+				
 			</div>
+			<div class="mt-5 flex flex-col gap-5 md:flex-row">
+				<div>
+					<label for="date" class="text-white mb-2 block">Want to check the past performance? Pick a date</label>
+						<input class="mt-3 w-full rounded-md p-1" type="date" name="date" bind:value={date} />
+				</div>
+					<button
+						formaction={`?/historical&conversion-rate=${conversionRate}`}
+						class="hidden h-6 w-6 shrink-0 self-end rounded-md bg-white p-1 hover:bg-slate-200 md:block">
+						<img src={time} alt="">
+						<span class="sr-only">Go back in time</span></button
+					>
+					{#if form?.historicalRate}
+						<p class="text-white self-end">Historical Rate: {historicalRate.toFixed(6)} ({(conversionRate - historicalRate).toFixed(6)})</p>
+						<p class="text-white"></p>
+					{/if}
+				</div>
 		{/if}
 	</form>
-	<!-- <form action="?/historical" method="post" use:enhance>
+	<!-- {#if form}
+		<form action="?/historical&base-currency=EUR&target-currency=USD" method="post" use:enhance>
 		<input hidden type="text" name="base-currency" bind:value={baseCurrency} />
 		<input hidden type="text" name="conversion-rate" bind:value={conversionRate} />
 		<input hidden type="text" name="target-currency" bind:value={targetCurrency} />
 		<input hidden type="text" name="amount-to-convert" bind:value={amountToConvert} />
-		<input class="mt-3 w-full rounded-md p-1" type="date" name="date" bind:value={date} />
+		
 		<p class="text-white">Historical rate {historicalRate}</p>
 		<button type="submit" class="w-5 bg-white text-white">button</button>
-	</form> -->
+	</form> 
+	{/if} -->
 </section>
